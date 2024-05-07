35ad3eae52e4e110788609a0bb0c92fc117d9638
thopp-tudelft <t.r.hoppener@tudelft.nl>
2024-05-07 18:20:45 +0200

    format exporter main

--- scripted_render_pipeline/exporter/__main__.py
+++ scripted_render_pipeline/exporter/__main__.py
@@ -2,6 +2,7 @@
 
 relies on provided parameters being set in the script for now
 """
+
 import logging
 import renderapi
 import requests
@@ -14,13 +15,14 @@ from ..basic_auth import load_auth
 HOST = "https://sonic.tnw.tudelft.nl"
 OWNER = "akievits"
 PROJECT = "20231107_MCF7_UAC_test"
-STACKS_2_EXPORT = ["postcorrection_rigid_scaled"] # list
-DOWNSCALING = 1 # Default
-DOWNSAMPLE = 7 # How many times to downsample data
-CONCURRENCY = 8 # Default number of processes
+STACKS_2_EXPORT = ["postcorrection_rigid_scaled"]  # list
+DOWNSCALING = 1  # Default
+DOWNSAMPLE = 7  # How many times to downsample data
+CONCURRENCY = 8  # Default number of processes
 PATH = Path(
     "/long_term_storage/webknossos/binaryData/hoogenboom-group/20231107_MCF7_UAC_test_test"
-    )
+)
+
 
 def _main():
     # Render authentication
@@ -28,13 +30,20 @@ def _main():
     session = requests.Session()
     session.auth = (USER, PASSWORD)
     render = dict(host=HOST, owner=OWNER, project=PROJECT, session=session)
-    
+
     # Get voxel size
     # Assumes equal voxel sizes
     # TODO: expand for stacks with different voxel sizes
-    stack_metadata = [renderapi.stack.get_stack_metadata(stack, **render) for stack in STACKS_2_EXPORT]
-    voxel_size = stack_metadata[0].stackResolutionX, stack_metadata[0].stackResolutionY, stack_metadata[0].stackResolutionZ
-    
+    stack_metadata = [
+        renderapi.stack.get_stack_metadata(stack, **render)
+        for stack in STACKS_2_EXPORT
+    ]
+    voxel_size = (
+        stack_metadata[0].stackResolutionX,
+        stack_metadata[0].stackResolutionY,
+        stack_metadata[0].stackResolutionZ,
+    )
+
     wk_exporter = Webknossos_Exporter(
         location=PATH,
         host=HOST,
@@ -43,11 +52,10 @@ def _main():
         downscaling=DOWNSCALING,
         voxel_size=voxel_size,
         downsample=DOWNSAMPLE,
-        concurrency=CONCURRENCY
+        concurrency=CONCURRENCY,
     )
-    wk_exporter.download_project(
-    stacks=STACKS_2_EXPORT
-    )
+    wk_exporter.download_project(stacks=STACKS_2_EXPORT)
+
 
 if __name__ == "__main__":
     logging.basicConfig(
